edc19884c96a7580932230c901f3f1f451b2c7b5
BakedPotato101 <jackson.baker7562@gmail.com>
2023-03-28 23:00:40 -0500

    added localStorage for themes

--- modules/theme.js
+++ modules/theme.js
@@ -3,16 +3,40 @@ const themeButtons = document.getElementsByClassName("theme-button");
 const themeToggleButton = document.getElementById("theme-toggle");
 const body = document.getElementsByTagName("body")[0];
 
+function saveTheme(theme) {
+  localStorage.setItem("theme", theme);
+}
+
+function loadTheme() {
+  const storedTheme = localStorage.getItem("theme");
+  if (storedTheme) {
+    body.classList.add(storedTheme);
+  } else {
+    // Set a default theme if no theme is stored
+    body.classList.add("default");
+    saveTheme("default");
+  }
+}
+
+function changeTheme(theme) {
+  // Remove existing theme classes
+  for (const button of themeButtons) {
+    body.classList.remove(button.getAttribute("data-theme"));
+  }
+
+  // Add the new theme class
+  body.classList.add(theme);
+  saveTheme(theme);
+}
+
 themeToggleButton.addEventListener("click", () => {
   themePanel.classList.toggle("open");
 });
 
 for (const button of themeButtons) {
   button.addEventListener("click", () => {
-    body.classList.remove("light");
-    body.classList.remove("default");
-    body.classList.remove("dark");
-    body.classList.remove("stupid");
-    body.classList.add(button.getAttribute("data-theme"));
+    changeTheme(button.getAttribute("data-theme"));
   });
 }
+
+loadTheme();
